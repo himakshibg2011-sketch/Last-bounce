b475965370629e8e5235d6c7ae9408eb76e36bc7
Sunshine and Rainbows <himakshi.b.g.2011@gmail.com>
2026-09-19 17:15:27 +0530

    Add game canvas and UI elements for Last Bounce

---
+++ script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const scoreText = document.getElementById("score");
+const timeText = document.getElementById("time");
+const livesText = document.getElementById("lives");
+
+const eventMessage = document.getElementById("event-Message");
+
+const gameOverScreen = document.getElementById("game-over");
